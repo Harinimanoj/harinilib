19939a9eeafb011f57c25937baa3353dc9c3c291
Harinimanoj <harinimanoj2010@gmail.com>
2025-05-31 02:18:58 +0530

    Update getInput.groovy

--- vars/getInput.groovy
+++ vars/getInput.groovy
@@ -1,4 +1,4 @@
-def (string name, string dayOfweek) {
+def call(string name,string dayOfweek) {
 
-     sh "echo hello ${name}. Todat is ${dayOfweek}."
+     sh "echo hello ${name}. Today is ${dayOfweek}."
 }
